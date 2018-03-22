138830f56bddc98c4b75c8a949aeadaa266ccbe3
Robert Ferney <rob@ferney.org>
2018-03-22 02:32:25 -0600

    switched to rspec, cucumber, and added devise

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,12 @@ module Trainer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.integration_tool :cucumber
+      g.test_framework :rspec
+      g.stylesheet_engine :sass
+      g.template_engine :haml
+    end
+
   end
 end
